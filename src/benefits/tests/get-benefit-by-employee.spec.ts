944ee8eb6a8afe80270f4a676cc907eaa3ee6bcc
Matheus Pessoa <matheus41.dev@gmail.com>
2026-04-30 17:36:16 -0300

    test: add unit tests for benefit module

---
+++ src/benefits/tests/get-benefit-by-employee.spec.ts
@@ -0,0 +1,65 @@
+import { AppDataSource } from 'src/config/database/data-source';
+import {
+  cleanupAll,
+  createBenefit,
+  getBenefitsByEmployee,
+  initTestDataSource,
+  setupDefaultEmployee,
+} from './helpers/benefit.helper';
+import { AuthHelper } from 'src/auth/tests/helpers/auth.helper';
+
+describe('GET /benefits/employee/:funcionarioId', () => {
+  let defaultEmployeeId: string;
+
+  beforeAll(async () => {
+    await AppDataSource.initialize();
+    initTestDataSource(AppDataSource);
+    await AuthHelper.setup(AppDataSource);
+    defaultEmployeeId = await setupDefaultEmployee();
+  });
+
+  beforeEach(async () => {
+    await cleanupAll();
+    defaultEmployeeId = await setupDefaultEmployee();
+  });
+
+  afterAll(async () => {
+    await cleanupAll();
+    await AppDataSource.destroy();
+  });
+
+  it('deve listar benefícios do funcionário com sucesso (200)', async () => {
+    const createRes = await createBenefit();
+    const { status, body } = await getBenefitsByEmployee(defaultEmployeeId);
+
+    console.log(createRes);
+    expect(status).toBe(200);
+    expect(body.succeeded).toBe(true);
+    expect(Array.isArray(body.data)).toBe(true);
+    expect(body.data?.length).toBeGreaterThan(0);
+    expect(body.data?.[0].FUNCIONARIO.ID).toBe(defaultEmployeeId);
+    expect(body.message).toBe(
+      'Benefícios de funcionário listados com sucesso.',
+    );
+  });
+
+  it('deve retornar lista vazia para funcionário sem benefício', async () => {
+    const { status, body } = await getBenefitsByEmployee(
+      '23c9639d-443d-457c-9892-f54ba7b267d7',
+    );
+
+    expect(status).toBe(200);
+    expect(body.succeeded).toBe(true);
+    expect(body.data?.length).toBe(0);
+  });
+
+  it('deve retornar 401 quando não autenticado', async () => {
+    const { status, body } = await getBenefitsByEmployee(
+      defaultEmployeeId,
+      false,
+    );
+
+    expect(status).toBe(401);
+    expect(body.succeeded).toBe(false);
+  });
+});
---
+++ src/benefits/tests/get-benefit.spec.ts
@@ -0,0 +1,45 @@
+import { AppDataSource } from 'src/config/database/data-source';
+import {
+  cleanupAll,
+  createBenefit,
+  getAllBenefits,
+  initTestDataSource,
+  setupDefaultEmployee,
+} from './helpers/benefit.helper';
+import { AuthHelper } from 'src/auth/tests/helpers/auth.helper';
+
+describe('GET /benefits', () => {
+  beforeAll(async () => {
+    await AppDataSource.initialize();
+    initTestDataSource(AppDataSource);
+    await AuthHelper.setup(AppDataSource);
+    await setupDefaultEmployee();
+  });
+
+  afterAll(async () => {
+    await cleanupAll();
+    await AppDataSource.destroy();
+  });
+
+  it('deve listar benefícios com sucesso (200)', async () => {
+    await createBenefit();
+    const { status, body } = await getAllBenefits();
+
+    expect(status).toBe(200);
+    expect(body.succeeded).toBe(true);
+    expect(Array.isArray(body.data)).toBe(true);
+    expect(body.data?.length).toBeGreaterThan(0);
+    expect(body.message).toBe('Benefícios listados com sucesso.');
+  });
+
+  it('deve retornar 401 quando não for autenticado', async () => {
+    const response = await fetch('http://localhost:3001/benefits', {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    const body = await response.json();
+    expect(response.status).toBe(401);
+    expect(body.succeeded).toBe(false);
+  });
+});
