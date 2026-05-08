c853c2bedd6e7910dd65a637037c204720c330f6
Matheus Pessoa <matheus41.dev@gmail.com>
2026-05-08 11:29:18 -0300

    feat(payroll): add payroll-slip service and export in module

--- src/payroll/payroll.module.ts
+++ src/payroll/payroll.module.ts
@@ -4,10 +4,12 @@ import { Payroll } from './entities/payroll.entity';
 import { Employee } from 'src/employees/entities/employee.entity';
 import { PayrollController } from './payroll.controller';
 import { PayrollService } from './payroll.service';
+import { PayrollSlipService } from './payroll-slip.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Payroll, Employee])],
   controllers: [PayrollController],
-  providers: [PayrollService],
+  providers: [PayrollService, PayrollSlipService],
+  exports: [PayrollService, PayrollSlipService],
 })
 export class PayrollModule {}
